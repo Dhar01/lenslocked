37a21ba1ddcd15cd425c1239d0aeadb453f0ca5f
Loknath Dhar <loknath-dhar@pm.me>
2024-03-02 17:41:44 +0600

    refactored

--- main.go
+++ main.go
@@ -12,11 +12,9 @@ import (
 
 type Router struct{}
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "contact.gohtml")
-
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
@@ -31,26 +29,14 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	// os agonistic
 	tplPath := filepath.Join("templates", "home.gohtml")
-
-	tpl, err := template.ParseFiles(tplPath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
-		return
-	}
-
+	executeTemplate(w, tplPath)
 }
 
 func galleryHandler(w http.ResponseWriter, r *http.Request) {
