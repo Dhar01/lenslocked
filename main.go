c11e0a830c95d0602a488f1747ab06009417437c
Loknath Dhar <loknath-dhar@pm.me>
2024-03-04 21:07:05 +0600

    using embed for build project

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"path/filepath"
 
 	"github.com/Dhar01/lenslocked/controllers"
+	"github.com/Dhar01/lenslocked/templates"
 	"github.com/Dhar01/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 )
@@ -15,15 +17,15 @@ func main() {
 	r := chi.NewRouter()
 
 	// home page
-	homeTpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	homeTpl := views.Must(views.ParseFs(templates.FS, "home.gohtml"))
 	r.Get("/", controllers.StaticHandler(homeTpl))
 
 	// contact page
-	contactTpl := views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	contactTpl := views.Must(views.ParseFs(templates.FS, "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(contactTpl))
 
 	// FAQ page
-	faqTpl := views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	faqTpl := views.Must(views.ParseFs(templates.FS, "faq.gohtml"))
 	r.Get("/faq", controllers.StaticHandler(faqTpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
--- views/template.go
+++ views/template.go
@@ -3,6 +3,7 @@ package views
 import (
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
@@ -27,6 +28,16 @@ func Parse(filepath string) (Template, error) {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
 
+	return Template{
+		htmlTpl: htmlTpl,
+	}, nil
+}
+
+func ParseFs(fs fs.FS, pattern string) (Template, error) {
+	htmlTpl, err := template.ParseFS(fs, pattern)
+	if err != nil {
+		return Template{}, fmt.Errorf("passing template: %w", err)
+	}
 	return Template{
 		htmlTpl: htmlTpl,
 	}, nil
